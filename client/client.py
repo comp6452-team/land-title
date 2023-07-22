f0a41c5035c32702ce202f822bc601cb9d3cea21
Yiwen <edven120722@gmail.com>
2023-07-22 16:34:48 +1000

    store data in off chain database

--- client/client.py
+++ client/client.py
@@ -1,6 +1,6 @@
 import json
 from web3 import Web3
-
+import sqlite3
 # Connect to a local Ethereum node (Ganache)
 web3 = Web3(Web3.HTTPProvider('http://localhost:8545'))
 
@@ -13,12 +13,44 @@ contract_address = truffleFile["networks"][latest_timestamp]["address"]
 
 # Load the contract
 contract = web3.eth.contract(address=contract_address, abi=contract_abi)
+def input_data():
+    property_details = {}
+    property_details['owner'] = input("Enter the name of the property owner: ")
+    property_details['address'] = input("Enter the address of the property: ")
+    property_details['area'] = input("Enter the area of the property in square feet: ")
+    # property_details['location'] = input("Enter the location of the property: ")
+    return property_details
+
+def store_data_in_db(token_id, property_details):
+
+
+    conn = sqlite3.connect('database.db')  # This will create a new sqlite file if it doesn't exist
+    cursor = conn.cursor()
+
+    # Create a new table if it doesn't exist
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS properties(
+            tokenId INTEGER PRIMARY KEY,
+            owner TEXT,
+            address TEXT,
+            area TEXT
+        )
+    ''')
+
+    # Insert the property details
+    cursor.execute('''
+        INSERT INTO properties(tokenId, owner, address, area) VALUES(?,?,?,?)
+    ''', (token_id, property_details['owner'], property_details['address'], property_details['area']))
+
+    conn.commit()
+    conn.close()
 
 def register_title(private_key):
-    data = {
-        "name": "randwick",
-        "price": 250
-    }
+    data = input_data()
+    # data = {
+    #     "name": "randwick",
+    #     "price": 250
+    # }
     # Convert data to hash string
     hash_data = web3.solidity_keccak(['string'], [json.dumps(data)])
 
@@ -46,7 +78,7 @@ def register_title(private_key):
 
     # Extract the token ID from the logs
     token_id = logs[0]['args']['tokenId']
-
+    store_data_in_db(token_id, data)
     print(f"Newly minted token ID: {token_id}")
     # print(f"Transaction receipt: {receipt}")
 
@@ -81,28 +113,13 @@ def transfer_title(token_id, to_address, from_private_key):
     return receipt
 
 def get_title(token_id):
-    contract.functions.getTitle()
+    response = contract.functions.getTitleDetails(token_id).call()
+    print(response)
 
-def verify_title(token_id, private_key):
-    account_address = web3.eth.account.from_key(private_key).address
-    # nonce = web3.eth.get_transaction_count(account_address)
-
-    # txn_dict = contract.functions.verifyTitle(int(token_id)).build_transaction({
-    #     'chainId': 1337, # Replace with your network's chainId
-    #     'gas': 500000,
-    #     'gasPrice': web3.to_wei('20', 'gwei'),
-    #     'nonce': nonce,
-    #     'from': account_address,
-    # })
-
-    # signed_txn = web3.eth.account.sign_transaction(txn_dict, private_key)
-
-    # # Send the transaction
-    # tx_hash = web3.eth.send_raw_transaction(signed_txn.rawTransaction)
-    # receipt = web3.eth.wait_for_transaction_receipt(tx_hash)
-    # # response = contract.functions.verifyTitle(token_id).call()
-    # print(receipt)
+def verify_title(token_id, account_address):
+    # account_address = web3.eth.account.from_key(private_key).address
     response = contract.functions.verifyTitle(token_id).call({'from': account_address})
+    print(response)
 
 
 if __name__ == "__main__":
@@ -130,7 +147,9 @@ if __name__ == "__main__":
             # transfer_title(token_id, receiver_address, private_key)
         elif command == "verify":
             token_id = input("Enter token id: ")
-
-            verify_title(int(token_id), private_key)
-
+            account_address = input("Enter account address: ")
+            verify_title(int(token_id), account_address)
+        elif command == "get":
+            token_id = input("Enter token id: ")
+            get_title(int(token_id))
     print("sender address: " + sender_address + "\nreceiver_address: " + receiver_address)
